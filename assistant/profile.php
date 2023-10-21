b68a289ede634aec044aef4d5ac3ef1026d862e0
Fern Aragon <95953827+FERN1701@users.noreply.github.com>
2023-10-21 22:05:28 +0800

    Updated roles guys

---
+++ assistant/profile.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<?php include "head.php";?>
+
+<body>
+   <div class="theme-loader">
+        <div class="loader-track">
+            <div class="preloader-wrapper">
+                <div class="spinner-layer spinner-blue">
+                    <div class="circle-clipper left">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="gap-patch">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="circle-clipper right">
+                        <div class="circle"></div>
+                    </div>
+                </div>
+                <div class="spinner-layer spinner-red">
+                    <div class="circle-clipper left">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="gap-patch">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="circle-clipper right">
+                        <div class="circle"></div>
+                    </div>
+                </div>
+
+                <div class="spinner-layer spinner-yellow">
+                    <div class="circle-clipper left">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="gap-patch">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="circle-clipper right">
+                        <div class="circle"></div>
+                    </div>
+                </div>
+
+                <div class="spinner-layer spinner-green">
+                    <div class="circle-clipper left">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="gap-patch">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="circle-clipper right">
+                        <div class="circle"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="pcoded" class="pcoded">
+        <div class="pcoded-overlay-box"></div>
+        <div class="pcoded-container navbar-wrapper">
+            <?php include "nav.php";?>
+
+            <div class="pcoded-main-container">
+                <div class="pcoded-wrapper">
+                    <?php include "sidebar.php";?>
+                    <div class="pcoded-content">
+                        <!-- Page-header start -->
+                        <div class="page-header">
+                            <div class="page-block">
+                                <div class="row align-items-center">
+                                    <div class="col-md-8">
+                                        <div class="page-header-title">
+                                            <h5 class="m-b-10">Profile</h5>
+                                            <p class="m-b-0"><?php echo $title ?></p>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <ul class="breadcrumb">
+                                            <li class="breadcrumb-item">
+                                                <a href="index.html"> <i class="fa fa-home"></i> </a>
+                                            </li>
+                                            <li class="breadcrumb-item"><a href="index.php">Dashboard</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Page-header end -->
+                        <div class="pcoded-inner-content">
+                            <!-- Main-body start -->
+                            <div class="main-body">
+                                <div class="page-wrapper">
+                                    <!-- Page-body start -->
+                                    <div class="page-body">
+                                        <div class="row">
+                                            <!--Contetnt -->
+                                            <div class="col-xl-12 col-md-12">
+                                                    <div class="card">
+                                                        <div class="card-header">
+                                                            Profile
+                                                        </div>
+
+                                                        <div class="row">
+                                                            <div class="col-sm-3">
+                                                            <div class="card-body">
+                                                                <div class="main-menu-header ml-4">
+                                                                <img class="img-radius" id="uploadButton" src="assets/images/avatar-4.jpg" alt="User-Profile-Image">
+                                                                </div>
+                                                        </div>
+                                                            </div>
+                                                            <div class="col-sm-8 mb-5 ">
+                                                                <form action="">
+                                                                    <div class="row">
+                                                                        <div class="col-sm-6">
+                                                                            <label for="">First Name</label>
+                                                                            <input type="text" name="" class="form-control" id="">
+                                                                        </div>
+                                                                        <div class="col-sm-6">
+                                                                            <label for="">Last Name</label>
+                                                                            <input type="text" name="" class="form-control" id="">
+                                                                        </div>
+                                                                        <div class="col-sm-6">
+                                                                            <label for="">Age</label>
+                                                                            <input type="text" name="" class="form-control" id="">
+                                                                        </div>
+                                                                        
+                                                                        <div class="col-sm-6">
+                                                                            <label for="">Address</label>
+                                                                            <input type="text" name="" class="form-control" id="">
+                                                                        </div>
+                                                                    </div> <br>
+                                                                    <p onclick="showSwalDialog()"  class="btn btn-primary">Submit</p>
+                                                                </form>
+                                                            </div>
+                                                        </div>
+                                                       
+                                                    </div>
+                                                    <div class="card">
+                                                        <div class="card-header">
+                                                            Administrative Credentials
+                                                        </div>
+                                                            <div class="col-sm-12 mb-5 mr-3">
+                                                                <form action="">
+                                                                    <div class="row">
+                                                                        <div class="col-sm-6">
+                                                                            <label for="">Email</label>
+                                                                            <input type="text" name="" class="form-control" id="">
+                                                                        </div>
+                                                                        <div class="col-sm-6">
+                                                                            <label for="">Contact Number</label>
+                                                                            <input type="text" name="" class="form-control" id="">
+                                                                        </div>
+                                                                        <div class="col-sm-6">
+                                                                            <label for="">Password</label>
+                                                                            <input type="text" name="" class="form-control" id="">
+                                                                        </div>
+                                                                        
+                                                                        <div class="col-sm-6">
+                                                                            <label for="">Repeat Password</label>
+                                                                            <input type="text" name="" class="form-control" id="">
+                                                                        </div>
+                                                                    </div> <br>
+                                                                    <p onclick="showSwalDialog()" class="btn btn-primary">Submit</p>
+                                                                </form>
+                                                            </div>
+                                                        </div>
+                                                       
+                                                    </div>
+                                            </div>  <!-- contetnt-->
+                                        </div>
+                                    </div>
+                                    <!-- Page-body end -->
+                                </div>
+                                <div id="styleSelector"> </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+   <?php include "footer.php"?>
+</body>
+
+</html>
---
+++ teacher/head.php
@@ -0,0 +1,27 @@
+<?php include "../control.php";?>
+<head>
+    <title><?php echo $title?></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+    <meta name="keywords" content="bootstrap, bootstrap admin template, admin theme, admin dashboard, dashboard template, admin template, responsive" />
+    <meta name="author" content="Codedthemes" />
+   <link rel="icon" href="../images/CCSlogo.png" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="assets/pages/waves/css/waves.min.css" type="text/css" media="all">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/pages/waves/css/waves.min.css" type="text/css" media="all">
+    <link rel="stylesheet" type="text/css" href="assets/icon/themify-icons/themify-icons.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome-n.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/jquery.mCustomScrollbar.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+    <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-regular-rounded/css/uicons-regular-rounded.css'>
+    
+   
+</head>
